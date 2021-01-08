5907b7e96e4fd4b126e172cb055d383994d2c8e3
patersam1 <sammo7531@gmailcom>
2021-01-09 11:24:35 +1300

    Initial commit

---
+++ Workout Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Workout Tracker
+//
+//  Created by Sam Paterson on 9/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
